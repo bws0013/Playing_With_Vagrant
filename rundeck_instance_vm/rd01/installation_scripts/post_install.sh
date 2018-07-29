011f677f123ad121534ece34fd9bd6ffd72a9080
Ben <i@auburn.edu>
2018-07-29 16:25:43 -0600

    now with rundeck actually working

--- rundeck_instance_vm/rd01/installation_scripts/post_install.sh
+++ rundeck_instance_vm/rd01/installation_scripts/post_install.sh
@@ -1,5 +1,20 @@
 #/bin/bash
 
+echo "----- REPLACING RUNDECK PROPERTIES ------"
+echo "-----------------------------------------"
+
+if [ -f /tmp/rundeck-config.properties ]; 
+then
+	cd /tmp
+	chmod 755 rundeck-config.properties
+	chown -R rundeck:rundeck rundeck-config.properties 
+	rm -f /etc/rundeck/rundeck-config.properties
+	mv rundeck-config.properties /etc/rundeck/	
+
+else
+	echo "No need to change the rundeck-config.properties"
+fi
+
 echo "---------- STARTING RUNDECK -------------"
 echo "-----------------------------------------"
 service rundeckd start
