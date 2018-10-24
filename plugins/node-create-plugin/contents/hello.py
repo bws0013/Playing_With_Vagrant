73f38bc226163094380768659e9b5100bb5ba3e8
Ben <i@auburn.edu>
2018-10-23 22:28:13 -0600

    figuring out command line flags in python

--- plugins/node-create-plugin/contents/hello.py
+++ plugins/node-create-plugin/contents/hello.py
@@ -1,1 +1,8 @@
-print("hello world")
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a node')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
