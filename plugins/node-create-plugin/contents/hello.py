0f87f5614ea68a3c18e9a0d48d6414de38759965
Ben <i@auburn.edu>
2018-10-24 09:09:19 -0600

    finished with command arg list hopefully

--- plugins/node-create-plugin/contents/hello.py
+++ plugins/node-create-plugin/contents/hello.py
@@ -3,13 +3,16 @@ import argparse
 parser = argparse.ArgumentParser(description='Create a node')
 # parser.add_argument('integers', metavar='N', type=int, nargs='+',
 #                     help='an integer for the accumulator')
-parser.add_argument("--hostname", "-H", help="Provide the hostname or ip of a node")
+parser.add_argument("--hostname", "-o", help="Provide the hostname or ip of a node")
 parser.add_argument("--node_name", "-n", help="Provide the name of the node (.yml will be added)")
 parser.add_argument("--project", "-p", help="Provide the hostname or ip of a node")
+parser.add_argument("--path", "-a", help="(optional) Provide the path of your node resource directory")
+parser.add_argument("--description", "-d", help="(optional) Provide a node description")
+parser.add_argument("--tags", "-t", help="(optional) Provide a comma separated list of tags (enter no spaces in the tag list)")
 
 args = parser.parse_args()
 
-hostname = node_name = project = None
+hostname = node_name = project = description = tags = None
 
 if args.hostname:
     hostname = args.hostname
