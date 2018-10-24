7bd72f167fa9545e8c2f8b7211a15f08c62dca24
Ben <i@auburn.edu>
2018-10-23 21:59:24 -0600

    taking progress, writing plugin requirements

--- plugins/node-create-pipeline.sh
+++ plugins/node-create-pipeline.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
 zip -r node-create-plugin.zip node-create-plugin/
+
+keypath='/Users/bensmith/Documents/Rundeck_Stuff/rundeck_instance_vm/rd01/.vagrant/machines/default/virtualbox/private_key'
+
+vagrant scp node-create-plugin.zip 3e10c49:/tmp
+
+cd /Users/bensmith/Documents/Rundeck_Stuff/rundeck_instance_vm/rd01
+vagrant ssh default -c "sudo mv /tmp/node-create-plugin.zip /var/lib/rundeck/libext/"
