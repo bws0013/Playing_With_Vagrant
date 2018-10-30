d1a999e802e09656277ac03ab7ab01f3ad02e0b0
Ben <i@auburn.edu>
2018-10-30 09:30:24 -0600

    adding xml gets wrong results

--- plugins/node-create-plugin/contents/add_node.py
+++ plugins/node-create-plugin/contents/add_node.py
@@ -1,18 +1,20 @@
 import xml.etree.ElementTree as ET
 
 def xml_add(args):
-
-    root = ET.parse(args.path).getroot()
+    print get_xml_template()
+    tree = ET.parse(args.path)
+    print "*************"
+    print tree
+    root = tree.getroot()
     child = ET.ElementTree(ET.fromstring(get_xml_template()))
     root.append(child)
     tree.write(args.path)
 
 
 def get_xml_template():
-    node_text = """
-    <node name=\"@node_name@\" hostname=\"@hostname@\""
-      description=\"@description@\""
-      tags=\"@tags@\" username=\"@username@\">"
-    </node>"
-    """
+    node_text = ""
+    node_text += "<node name=\"@node_name@\" hostname=\"@hostname@\""
+    node_text += " description=\"@description@\""
+    node_text += " tags=\"@tags@\" username=\"@username@\">"
+    node_text += "</node>"
     return node_text
