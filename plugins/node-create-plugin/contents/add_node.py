3301c55f7192b1c377a892bc6b93624b66ba1f21
Ben <i@auburn.edu>
2018-10-30 21:07:58 -0600

    added xml checking

--- plugins/node-create-plugin/contents/add_node.py
+++ plugins/node-create-plugin/contents/add_node.py
@@ -1,20 +1,24 @@
 import xml.etree.ElementTree as ET
 
 def xml_add(args):
-    print get_xml_template()
-    tree = ET.parse(args.path)
-    print "*************"
-    print tree
+    node_text = get_xml_template()
+    node_text = node_text.replace("@node_name@", args.node_name)
+    node_text = node_text.replace("@description@", args.description)
+    node_text = node_text.replace("@hostname@", args.hostname)
+    node_text = node_text.replace("@username@", args.username)
+    node_text = node_text.replace("@tags@", args.tags)
+
+    import xml.etree.ElementTree as ET
+    tree = ET.parse('output/resources.xml')
     root = tree.getroot()
-    child = ET.ElementTree(ET.fromstring(get_xml_template()))
-    root.append(child)
-    tree.write(args.path)
+    for child in root.findall('node'):
+        print child.get('name')
+
 
 
 def get_xml_template():
-    node_text = ""
+    node_text = "  "
     node_text += "<node name=\"@node_name@\" hostname=\"@hostname@\""
     node_text += " description=\"@description@\""
-    node_text += " tags=\"@tags@\" username=\"@username@\">"
-    node_text += "</node>"
+    node_text += " tags=\"@tags@\" username=\"@username@\"></node>"
     return node_text
